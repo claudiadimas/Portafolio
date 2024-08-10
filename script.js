e1acd1f696677295696e0cab30eb86058baa724c
claudiadimas <claudiadimas35@gmail.com>
2024-08-09 19:50:08 -0600

    Cambios typewriter

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let typewriter = new Typewriter(app, {
 
 typewriter
     .pauseFor(1000)
-    .typeString('Claudia Dimas')
+    .typeString('Habilidades')
     .pauseFor(300)
     .deleteChars(10)
 
@@ -15,21 +15,19 @@ typewriter
     .pauseFor(1000)
     .start();
 
-
-
-let frase = document.getElementById('frase');
-let typewriterFrase = new Typewriter(frase, {
-    loop: true,
-    delay: 75,
-});
-
-typewriterFrase
-    .pauseFor(1000)
-    .typeString('Ingeniera Mecatrónica muy resiliente, honesta, respetuosa, muy proactiva, con capacidad de crear y encontrar soluciones a cualquier problema, y ​​una persona que considera fundamental el trabajo en equipo y la buena convivencia.Aprendo de una manera rápida y estoy muy motivada para superarme día a día.')
-    .pauseFor(300)
-    .deleteChars(10)
-
-    //   .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-    //   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-    .pauseFor(1000)
-    .start();
+    let app2 = document.getElementById('app2');
+    let typewriter2 = new Typewriter2(app2, {
+        loop: true,
+        delay: 75,
+    });
+    
+    typewriter2
+        .pauseFor(1000)
+        .typeString('Proyectos')
+        .pauseFor(300)
+        .deleteChars(10)
+    
+        //   .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
+        //   .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+        .pauseFor(1000)
+        .start();
